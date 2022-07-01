0a3d1a785b1df27826be300faf39f445c9426247
Fer Rodríguez <100962630+XxSinIdeasxX@users.noreply.github.com>
2022-06-30 23:25:49 -0500

    Delete SO Practica 1 directory

--- SO Practica 1/Ejercicio7.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Ejercicio 7
-echo "~ Ejercicio 7 ~"
-
-if [ $# -eq 0 ]; then
-echo "Sin argumentos";
-else echo "Argumentos: $*";
-fi
--- SO Practica 1/Ejercicio9.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Ejercicio 8
-echo "~ Ejercicio 8 ~"
-
-for i in $(tr ' ' '_' </etc/passwd)
-do
-set $(echo $i | tr ':' ' ')
-echo user: $1, UID: $3, Home Directory: $6
-done
